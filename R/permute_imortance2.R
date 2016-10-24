8a6bc92a31c1c451f5d792b7a282c5027b6c8bb4
natydasilva <ndasilva@iastate.edu>
2016-10-24 00:10:33 -0500

    update permute_imortance2

--- R/permute_imortance2.R
+++ R/permute_imortance2.R
@@ -9,7 +9,12 @@
 #' pprf.crab<- PPforest(data = crab, class = "Type",
 #'  size.tr = 1, m = 100, size.p = .4, PPmethod = 'LDA', strata = TRUE)
 #' permute_importance2(ppf = pprf.crab) 
+#' 
 permute_importance2 <- function(ppf){
+  sd <-NULL
+  imp <- NULL
+  imp2 <- NULL
+  
   train <- as.matrix(ppf$train[, -which(colnames(ppf$train) == ppf$class.var)])
   classes <- as.integer(ppf$train[,which(colnames(ppf$train) == ppf$class.var)])
   oobid <- apply(ppf$oob.obs, 1, function(x) which(x) - 1) 
@@ -26,12 +31,13 @@ permute_importance2 <- function(ppf){
   rank.var <- t(apply(corr.oob.per, 1, rank, ties.method = "random"))
   corr.oob <- (1-ppf$oob.error.tree) * unlist(lapply(oobid, length))
   n.oob <-  unlist(lapply(permute, length))
-  imp.pl <- data.frame( nm = colnames(ppf$train)[colnames(ppf$train)!=ppf$class.var], 
-                        imp=apply(corr.oob.per, 2, function(x) mean((corr.oob-x))), 
-                        imp2=apply(corr.oob.per, 2, function(x) mean(((1-x/n.oob)-ppf$oob.error.tree))), 
-                        sd =apply(corr.oob.per, 2, function(x) sd( ((1-x/n.oob)-ppf$oob.error.tree)))) %>%
-    dplyr::mutate(imp.sd =imp2/sd)%>% dplyr::arrange(imp)
   
+  imp.pl <- data.frame( nm = colnames(ppf$train)[colnames(ppf$train)!= ppf$class.var], 
+                        imp = apply(corr.oob.per, 2, function(x) mean((corr.oob-x))), 
+                        imp2 = apply(corr.oob.per, 2, function(x) mean(((1-x/n.oob)-ppf$oob.error.tree))), 
+                        sd = apply(corr.oob.per, 2, function(x) sd( ((1-x/n.oob)-ppf$oob.error.tree)))) %>%
+    
+    dplyr::mutate(imp.sd = imp2/sd)%>% dplyr::arrange(imp)
   imp.pl$nm <- factor(imp.pl$nm, levels= imp.pl[order( imp.pl$imp2), "nm"])
   
   imp.pl
