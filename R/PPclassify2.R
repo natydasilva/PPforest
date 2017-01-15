2ac98a43b0f44d59ceef3eeedce3fc5c0690fed7
natydasilva <ndasilva@iastate.edu>
2017-01-14 20:53:21 -0600

    PPclassify2 replaces PP.classify form PPtreeViz, main functions are in RCPP

---
+++ R/PPclassify2.R
@@ -0,0 +1,56 @@
+
+#' Predict class for the test set and calculate prediction error
+#' 
+#' After finding tree structure, predict class for the test set and calculate prediction error.
+#' @usage PPclassify2( Tree.result, test.data, Rule, true.class)  
+#' @param Tree.result the result of PP.Tree
+#' @param test.data  the test dataset
+#' @param true.class true class of test dataset if available
+#' @param Rule split rule 1:mean of two group means, 2:weighted mean, 3: mean of max(left group) and min(right group), 4: weighted mean of max(left group) and min(right group)
+#' @return predict.class predicted class
+#' @return predict.error prediction error
+#' @references Lee, YD, Cook, D., Park JW, and Lee, EK(2013) 
+#' PPtree: Projection pursuit classification tree, 
+#' Electronic Journal of Statistics, 7:1369-1386.
+#' @export
+#' @keywords tree
+
+PPclassify2 <- function (Tree.result, test.data = NULL, Rule = 1, true.class = NULL, 
+                        ...) 
+{
+  if (is.null(test.data)) 
+    test.data <- Tree.result$origdata
+  test.data <- as.matrix(test.data)
+  if (!is.null(true.class)) {
+    true.class <- as.matrix(true.class)
+    if (nrow(true.class) == 1) 
+      true.class <- t(true.class)
+    if (!is.numeric(true.class)) {
+      class.name <- names(table(true.class))
+      temp <- rep(0, nrow(true.class))
+      for (i in 1:length(class.name)) temp <- temp + (true.class == 
+                                                        class.name[i]) * i
+      true.class <- temp
+    }
+  }
+ 
+  
+  n <- nrow(test.data)
+  class.temp <- rep(1, n)
+  test.class.index <- matrix(rep(0,n), ncol=n)
+  temp <- PPclassindex(class.temp, test.class.index, as.matrix(test.data), 
+                       as.matrix(Tree.result$Tree.Struct), as.matrix(Tree.result$projbest.node), as.matrix(Tree.result$splitCutoff.node), 
+                       0, Rule)
+  test.class <- rep(0, n)
+  IOindex <- rep(1, n)
+  temp <- PPclassification(as.matrix(Tree.result$Tree.Struct), as.matrix(temp$testclassindex[-1,]), 
+                           as.vector(IOindex), as.vector(test.class), 0, 0)
+  if (!is.null(true.class)) {
+    predict.error <- sum(true.class != temp$test.class)
+  }else{
+    predict.error <- NA
+  }
+  class.name <- names(table(Tree.result$origclass))
+  predict.class <- class.name[temp$testclass]
+  list(predict.error = predict.error, predict.class = predict.class)
+}
