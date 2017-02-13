ac906c5b106040c7aa5c189a69e0700c04c1bbd4
Natalia da Silva <natydasilva@dyn-49-127-129-75.its.monash.edu.au>
2017-02-14 10:40:01 +1100

    update example viz

--- R/ternary_str.R
+++ R/ternary_str.R
@@ -12,7 +12,27 @@
 #' #crab data set with all the observations used as training
 #' pprf.crab <- PPforest(data = crab, std =TRUE, class = "Type",
 #'  size.tr = 1, m = 200, size.p = .5, PPmethod = 'LDA')
-#' ternary_str ( pprf.crab, id = c(1,2,3), sp = 3, dx = 1, dy = 2) 
+#'  
+#' pl_ter <- function(dat, dx, dy ){
+#'   p1  <- dat[[1]] %>% dplyr::filter(pair %in% paste(dx, dy, sep = "-") ) %>%
+#'     dplyr::select(Class, x, y) %>%
+#'     ggplot2::ggplot(ggplot2::aes(x, y, color = Class)) +
+#'     ggplot2::geom_segment(data = dat[[2]], ggplot2::aes(x = x1, xend = x2,
+#'                                                y = y1, yend = y2), color = "black" ) +
+#'     ggplot2::geom_point(size = I(3), alpha = .5) +
+#'     ggplot2::labs(y = " ",  x = " ") +
+#'     ggplot2::theme(legend.position = "none", aspect.ratio = 1) +
+#'     ggplot2::scale_colour_brewer(type = "qual", palette = "Dark2") +
+#'     ggplot2::labs(x = paste0("T", dx, ""), y = paste0("T", dy, " ")) +
+#'     ggplot2::theme(aspect.ratio = 1)
+#'   p1
+#' }
+#' #ternary plot in tree different selected dierections
+#' p1 <- pl_ter(ternary_str(pprf.crab, id = c(1, 2, 3), sp = 3, dx = 1, dy = 2), 1, 2 )
+#' p2 <-  pl_ter(ternary_str(pprf.crab, id = c(1, 2, 3), sp = 3, dx = 1, dy = 3), 1, 3)
+#' p3 <-  pl_ter(ternary_str(pprf.crab, id = c(1, 2, 3), sp = 3, dx = 2, dy = 3), 2, 3)
+#' gridExtra::grid.arrange(p1, p2, p3, ncol = 3)
+#' 
 ternary_str <-  function(ppf, id, sp, dx, dy){
   x <- NULL
   y <- NULL
