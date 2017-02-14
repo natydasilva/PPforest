0e218796a73a356b9d9f29f2a25bc972ec3ca231
Natalia da Silva <natydasilva@dyn-49-127-129-75.its.monash.edu.au>
2017-02-15 10:31:59 +1100

    include olive data

---
+++ R/olive.R
@@ -0,0 +1,21 @@
+#' The olive data set 
+#'
+#' contains  572 observations and 10 variables
+#' \itemize{
+#' \item{Region}{Three super-classes of Italy: North, South and the island of Sardinia }
+#' \item{area}{ Nine collection areas: three from North, four from South and 2 from Sardinia} 
+#' \item{palmitic}{ fatty acids percent x 100}
+#' \item{palmitoleic}{ fatty acids percent x 100 }
+#' \item{stearic}{fatty acids percent x 100}
+#' \item{oleic}{fatty acids percent x 100 }
+#' \item{linoleic}{fatty acids percent x 100}
+#' \item{linolenic}{fatty acids percent x 100  }
+#' \item{arachidic}{ fatty acids percent x 100}
+#' \item{eicosenoic}{ fatty acids percent x 100}
+#' }
+#' @docType data
+#' @keywords datasets
+#' @name olive
+#' @usage data(olive)
+#' @format A data frame contains 573 observations and 10 variables
+NULL 
