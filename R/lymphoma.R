e055b5f1f960e5340f2fde2e79e9932d15cfce89
natydasilva <natalia.dasilva@fcea.edu.uy>
2025-07-23 18:22:41 -0300

    fixed typo

--- R/lymphoma.R
+++ R/lymphoma.R
@@ -3,7 +3,7 @@
 #' Gene expression in the three most prevalent adult lymphoid malignancies: B-cell chronic lymphocytic leukemia (B-CLL), follicular lymphoma (FL), and diffuse large B-cell lym- phoma (DLBCL). Gene expression levels were measured using a specialized cDNA microarray, the Lymphochip, containing genes that are preferentially expressed in lymphoid cells or that are of known immunologic or oncologic importance. This data set contain 80 observations from 3 lymphoma types.
 #' \describe{
 #'   \item{Type}{ Class variable has 3 classes with 29 cases of B-cell ALL (B-CLL), 42 cases of diffuse large B-cell lymphoma (DLBCL) and 9 cases of follicular lymphoma (FL).} 
-#'   \item{Gene1 to Gen 50}{gene expression.}
+#'   \item{Gene1, Gen2, ..., Gen50}{gene expression.}
 #'   }
 #' @docType data
 #' @keywords datasets
