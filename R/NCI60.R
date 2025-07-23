c0393260819bd883ac8008612b49b144bd54bd43
natydasilva <natalia.dasilva@fcea.edu.uy>
2025-07-23 18:44:28 -0300

    update documentation

--- R/NCI60.R
+++ R/NCI60.R
@@ -1,9 +1,9 @@
 #' NCI60 data set
 #' 
-#'cDNA microarrays were used to examine the variation in gene expression among the 60 cell lines.  The cell lines are derived from tumors with different sites of origin. This data set contain 61 observations and 30 feature variables from 8 different tissue types.
+#' @format cDNA microarrays were used to examine the variation in gene expression among the 60 cell lines.  The cell lines are derived from tumors with different sites of origin. This data set contain 61 observations and 30 feature variables from 8 different tissue types.
 #' \describe{
-#'   \item{Type}{ has 8 different tissue types, 9 cases of breast, 5 cases of central nervous system (CNS), 7 cases pf colon, 8 cases of leukemia, 8 cases of melanoma, 9 cases of  non-small-cell lung carcinoma (NSCLC), 6 cases of ovarian and 9 cases of renal.} 
-#'   \item{Gene1, Gen2, ..., Gen30}{Numeric gene expression information}
+#'   \item{Type}{has 8 different tissue types, 9 cases of breast, 5 cases of central nervous system (CNS), 7 cases pf colon, 8 cases of leukemia, 8 cases of melanoma, 9 cases of  non-small-cell lung carcinoma (NSCLC), 6 cases of ovarian and 9 cases of renal.} 
+#'   \item{Gene1, Gen2, ..., Gen30}{Numeric gene expression information.}
 #'   }
 #' @docType data
 #' @keywords datasets
